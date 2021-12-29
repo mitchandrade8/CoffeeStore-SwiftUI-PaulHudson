c265dcc7e00bf7bd9bb5dc6911a9ff3c347d4f76
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2021-12-28 17:59:40 -0700

    Initial Commit

---
+++ CoffeeAppWithPaul/CoffeeAppWithPaulApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppWithPaulApp.swift
+//  CoffeeAppWithPaul
+//
+//  Created by Mitch Andrade on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppWithPaulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
